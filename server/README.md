3541618b9619ba11c820234b028a4e39e842919c
George Leyva <leyvageorge@hotmail.com>
2023-05-26 16:48:50 -0700

    added connection to postgres

---
+++ server/README.md
@@ -0,0 +1,2 @@
+Notes:
+Create name for database. also test with code in server.js will comment out.
---
+++ server/server.js
@@ -0,0 +1,38 @@
+// DEPENDENCIES
+const express = require('express')
+const app = express()
+
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+// SEQ Dependencies
+const { Sequelize } = require('sequelize')
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: process.env.PASSWORD
+  })
+  
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+
+// ROOT
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the Tour API'
+    })
+})
+
+// LISTEN
+app.listen(process.env.PORT, () => {
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
+})
